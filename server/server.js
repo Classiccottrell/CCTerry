ab14b2a7edba4466bb7a22202fa92c737ebb909a
Matt <matt@Tylers-Macbook-Pro.local>
2016-05-06 17:44:33 -0700

    fix

--- server/server.js
+++ server/server.js
@@ -22,7 +22,6 @@ app.set('port', (process.env.PORT || 5000));
 //start listening on port
 app.listen(app.get('port'), function () {
 	console.log('Node app is running at: ' + colors.green('localhost:' + app.get('port')));
-	console.log('Tagga Proxy config', taggaProxy);
 });
 
 app.use(express.static('build'));
