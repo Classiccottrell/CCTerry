0fadaeaef549d303e6c0518778db117ae319dc72
Matt <matt@Tylers-Macbook-Pro.local>
2017-02-27 13:18:12 -0800

    updated pdf

--- app/app.js
+++ app/app.js
@@ -38,3 +38,20 @@ function GlobalConfig($locationProvider, $urlRouterProvider) {
 	// $locationProvider.html5Mode(true);
 	$urlRouterProvider.otherwise('/');
 }
+
+// Generate colors (as Chroma.js objects)
+var colors = paletteGenerator.generate(
+  6, // Colors
+  function(color){ // This function filters valid colors
+    var hcl = color.hcl();
+    return (hcl[0]>=59 || hcl[0]<=290)
+      && hcl[1]>=44.02 && hcl[1]<=98.37
+      && hcl[2]>=47.83 && hcl[2]<=82.17;
+  },
+  true, // Using Force Vector instead of k-Means
+  50, // Steps (quality)
+  false, // Ultra precision
+  'Default' // Color distance type (colorblindness)
+);
+// Sort colors by differenciation first
+colors = paletteGenerator.diffSort(colors, 'Default');
