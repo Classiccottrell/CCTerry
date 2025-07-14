ac7b7d2d37e888ff2d3d0826acea0f1f7122b780
Matt Cottrell <classiccottrell@gmail.com>
2025-07-14 10:45:49 -0700

    Move unused assets to unused directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,82 +1,104 @@
-var path = require('path');
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-var dir_js = path.resolve(__dirname, 'app');
-var dir_assets = path.resolve(__dirname, 'app', 'assets');
-var dir_build = path.resolve(__dirname, 'dist');
+const dir_js = path.resolve(__dirname, 'app');
+const dir_build = path.resolve(__dirname, 'dist');
 
 module.exports = {
-		entry: {
-			app : path.resolve(__dirname, 'index.js')
-		},
-		output: {
-				path: dir_build,
-				filename: 'bundle.js'
-		},
-		resolve: {
-			 modulesDirectories: ['node_modules', dir_js],
-		},
-		devServer: {
-				contentBase: dir_build,
-		},
-		postcss: function () {
-				return [require('autoprefixer')];
-		},
-		devtool: 'source-map',
-		stats: {
-				colors: true,
-				chunkModules: false
-		},
-		plugins: [
-				new ExtractTextPlugin('[name].css', {	allChunks: true }),
-				new webpack.optimize.CommonsChunkPlugin('vendor', '[name].js'),
-				new webpack.NoErrorsPlugin(),
-				new CopyWebpackPlugin([
-					{
-						from: './app/assets',
-						to: 'assets'
-					}
-				]),
-				new CopyWebpackPlugin([
-					{
-						from: './node_modules/font-awesome/fonts',
-						to: 'fonts'
-					}
-				]),
-				new webpack.ProvidePlugin({
-				    $: "jquery",
-				    jQuery: "jquery",
-				    "window.jQuery": "jquery"
-				})
-		],
-		module: {
-				loaders: [
-						{
-								loader: 'file?name=assets[name].[ext]',
-								test: /\.png($|\?)|\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/
-						},
-						{
-								loader: 'babel-loader',
-								test: /\.js$/,
-								exclude: /node_modules/,
-								presets : ['es2015']
-						},
-						{
-								loader: 'file?name=/[name].html',
-								test: /index.html$/
-						},
-						{
-								loader: 'raw',
-								test: /\.html$/,
-								exclude: /index.html$/
-						},
-						{
-								// loader: ExtractTextPlugin.extract('style-loader', 'css-loader?sourceMap!postcss-loader!less-loader?sourceMap'),
-								loader: ExtractTextPlugin.extract('style-loader', 'raw-loader?sourceMap!postcss-loader!less-loader?sourceMap'),
-								test: /\.less$/
-						}
-				]
-		}
-}
+  entry: {
+    app: path.resolve(__dirname, 'index.js'),
+  },
+  output: {
+    path: dir_build,
+    filename: 'bundle.js',
+  },
+  resolve: {
+    modules: ['node_modules', dir_js],
+  },
+  devServer: {
+    static: dir_build,
+  },
+  devtool: 'source-map',
+  stats: {
+    colors: true,
+    chunkModules: false,
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: './app/assets',
+          to: 'assets',
+        },
+        {
+          from: './node_modules/font-awesome/fonts',
+          to: 'fonts',
+        },
+      ],
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[name][ext]',
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /index\.html$/,
+        type: 'asset/resource',
+        generator: {
+          filename: '[name][ext]',
+        },
+      },
+      {
+        test: /\.html$/,
+        exclude: /index\.html$/,
+        loader: 'raw-loader',
+      },
+      {
+        test: /\.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'less-loader',
+        ],
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+};
